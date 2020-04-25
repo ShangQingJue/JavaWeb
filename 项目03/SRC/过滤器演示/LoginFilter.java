10f2a6207403743393a257acc31084b9a1d448a9
ShangQingJue <43850434+ShangQingJue@users.noreply.github.com>
2020-04-25 20:30:52 +0800

    Create LoginFilter.java

---
+++ 项目03/SRC/过滤器演示/LoginFilter.java
@@ -0,0 +1,33 @@
+package filterdemo;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/protected/*")
+public class LoginFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+        HttpSession session = request.getSession();
+        if ((String)session.getAttribute("id") != null) {
+            filterChain.doFilter(request, response);
+        }else {
+            response.sendRedirect("../login.html");
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
