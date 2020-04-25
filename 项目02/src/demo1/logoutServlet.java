477282f44cc3b78a641ade967f70c952b6fa66e2
ShangQingJue <43850434+ShangQingJue@users.noreply.github.com>
2020-04-25 21:00:25 +0800

    Create logoutServlet.java

---
+++ 项目02/src/demo1/logoutServlet.java
@@ -0,0 +1,27 @@
+package demo1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class logoutServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie: cookies) {
+            if (cookie.getName().equals("name")) {
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+                response.sendRedirect("./login1.html");
+            }
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
